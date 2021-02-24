cbb1080cbd49ff2ec04a1d087a30fc56325e8eeb
Danil Shamsiev <veismahs@outlook.com>
2021-02-24 22:34:17 +0300

    new logger

---
+++ src/NEW/Logger.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module NEW.Logger where
+
+import           Data.Aeson (FromJSON (..), withText)
+import           Data.Text  (unpack)
+import           Prelude    hiding (error)
+
+data Severity
+  = Debug
+  | Info
+  | Warning
+  | Error
+  deriving (Eq, Ord)
+
+instance Show Severity where
+  show Debug   = "[ DEBUG ] "
+  show Info    = "[ INFO  ] "
+  show Warning = "[ WARN  ] "
+  show Error   = "[ ERROR ] "
+
+data Handle =
+  Handle
+    { debug   :: String -> IO ()
+    , info    :: String -> IO ()
+    , warning :: String -> IO ()
+    , error   :: String -> IO ()
+    }
+
+instance FromJSON Severity where
+  parseJSON =
+    withText "FromJSON Logger.Severity" $ \t ->
+      case t of
+        "debug"   -> pure Debug
+        "info"    -> pure Info
+        "warning" -> pure Warning
+        "error"   -> pure Error
+        _         -> fail $ "Unkown severity: " ++ unpack t
