66ee6f9162b60e23647f2a24371a4ef70f5f8183
Danil Shamsiev <veismahs@outlook.com>
2021-02-28 01:54:38 +0300

    Started vk

---
+++ src/Bot/VK.hs
@@ -0,0 +1,25 @@
+module Bot.VK where
+
+import Bot
+import Data.IORef
+import qualified Data.Map.Strict as M
+import Data.Text (Text)
+import qualified Logger
+import qualified Web
+
+--------------------------------------------------------------------------------
+type Counter = Int
+
+type Counters = M.Map ChatId Counter
+
+--------------------------------------------------------------------------------
+data IHandle =
+  IHandle
+    { iAccessKey :: Text
+    , iGroupId :: Text
+    , iApiVersion :: Text
+    , iHelpMessage :: Text
+    , iRepeatCount :: Text
+    , iCounters :: IORef Counters
+    , iLogger :: Logger.Handle
+    }
