4648631cb4ed5c253dc86266d4cbad85099b2b23
Danil Shamsiev <veismahs@outlook.com>
2021-03-05 04:12:49 +0300

    Used TF logger in main - it works

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Monad.IO.Class (liftIO)
+import qualified Logger
+import qualified Logger.StdLogger as StdLogger
+
 main :: IO ()
-main = undefined
+main = do
+  let config = StdLogger.Config Logger.Debug
+  liftIO $ StdLogger.runStdLogger (Logger.debug "Log message") config
